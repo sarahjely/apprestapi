19ba18596373971e3fc3d096611fca507e96435c
sarah <sarahjelyy@gmail.com>
2024-09-08 00:14:05 +0700

    berhasil menampilkan data berdasarkan id

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { json } = require('body-parser');
+
 module.exports = function(app){
     var jsonku = require('./controller');
 
@@ -8,4 +10,7 @@ module.exports = function(app){
 
     app.route('/tampil')
     .get(jsonku.tampilsemuamahasiswa);
+
+    app.route('/tampil/:id')
+    .get(jsonku.tampilberdasarkanid)
 };
